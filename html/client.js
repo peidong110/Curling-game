881fa714f46c0a7a6a7b0ddaf0a0acd84b32c9b4
yumizhtch Corn// <yumizhtch@hotmail.com>
2018-11-06 23:32:14 -0500

    draw balls update

--- html/client.js
+++ html/client.js
@@ -2,6 +2,15 @@ let socket = io("http://" + window.document.location.host)
 let canvas1 = document.getElementById("canvas1")
 let canvas2 = document.getElementById("canvas2")
 
+let balls = [
+  {name:'ball1', x:170, y:500, color:'yellow'},
+  {name:'ball2', x:130, y:90, color:'yellow'},
+  {name:'ball3', x:77, y:420, color:'yellow'},
+  {name:'ball4', x:50, y:60, color:'red'},
+  {name:'ball5', x:18, y:300, color:'red'},
+  {name:'ball6', x:180, y:570, color:'red'}
+]
+
 function drawCanvas() {
   //draw left canvas1
   var context1 = canvas1.getContext('2d');
@@ -17,9 +26,28 @@ function drawCanvas() {
   drawOvalShape(context1, canvas1.width/2, canvas1.height/2, 60, 60);
   context1.fillStyle = '#ffffff';
   context1.fill();
+
   //draw right canvas2
   var context2 = canvas2.getContext('2d');
   context2.drawImage(canvas1, 0,0,canvas1.width,canvas1.height,0,0,canvas2.width,canvas2.height/3);
+
+  //draw balls
+  for (ball of balls){
+    var context2 = canvas2.getContext('2d');
+    drawOvalShape(context2, ball.x, ball.y, 15, 15);
+    context2.fillStyle = '#525050';
+    context2.fill();
+
+    drawOvalShape(context2, ball.x, ball.y, 8, 8);
+    if(ball.color == 'yellow'){
+      context2.fillStyle = '#ff0000';
+      context2.fill();
+    }
+    if(ball.color == 'red'){
+      context2.fillStyle = '#ffff00';
+      context2.fill();
+    }
+  }
 }
 
 function drawOvalShape(context, center_x, center_y, width, height){
